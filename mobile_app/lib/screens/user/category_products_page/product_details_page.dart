28a4112d80fee5794b9607fef68f6393fb8ae017
Marcin Leśniak <mcjajecznik@gmail.com>
2024-01-11 21:40:03 +0100

    ProductDetailsPage rework

--- mobile_app/lib/screens/user/category_products_page/product_details_page.dart
+++ mobile_app/lib/screens/user/category_products_page/product_details_page.dart
@@ -37,17 +37,36 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
     return loadedImages;
   }
 
-  ListTile buildListTile(String label, String content, IconData icon) {
-    return ListTile(
-      leading: Icon(icon),
-      title: Row(
+  Container buildListTile(String label, String content, bool color) {
+    return Container(
+      color: color ? Colors.grey.withOpacity(0.3) : null,
+      padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+      child: Row(
         children: [
-          Text(
-            '$label: ',
-            style: const TextStyle(fontWeight: FontWeight.bold),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Text(
+                '$label: ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
           ),
-          Text(
-            content,
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Text(
+                content,
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
           ),
         ],
       ),
@@ -102,159 +121,134 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                       }
                     },
                   ),
-                  const SizedBox(height: 16.0),
-                  Text('Cena: ${widget.price} zł',
-                      style: const TextStyle(fontSize: 18.0)),
-                  const SizedBox(height: 16.0),
-                  const Text(
-                    'Szczegóły:',
-                    style:
-                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  Padding(
+                    padding: EdgeInsets.only(top: 12.0, bottom: 6.0),
+                    child: Text('Cena: ${widget.price} zł',
+                        style: const TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 6.0, bottom: 6.0),
+                    child: const Text(
+                      'Szczegóły produktu:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                   if (widget.categoryId == 0) ...[
                     buildListTile(
-                        'Wyświetlacz', widget.details['display'], Icons.tv),
-                    buildListTile('Rozdzielczość', widget.details['resolution'],
-                        Icons.aspect_ratio),
+                        'Wyświetlacz', widget.details['display'], true),
+                    buildListTile(
+                        'Rozdzielczość', widget.details['resolution'], false),
                     buildListTile('Odświeżanie ekranu',
-                        widget.details['refreshing'], Icons.refresh),
+                        widget.details['refreshing'], true),
+                    buildListTile('Aparat', widget.details['camera'], false),
+                    buildListTile('Procesor', widget.details['cpu'], true),
+                    buildListTile('Pamieć RAM', widget.details['ram'], false),
+                    buildListTile('Pojemność', widget.details['storage'], true),
                     buildListTile(
-                        'Aparat', widget.details['camera'], Icons.camera),
+                        'System operacyjny', widget.details['system'], false),
                     buildListTile(
-                        'Procesor', widget.details['cpu'], Icons.memory),
-                    buildListTile(
-                        'Pamieć RAM', widget.details['ram'], Icons.memory),
-                    buildListTile(
-                        'Pojemność', widget.details['storage'], Icons.storage),
-                    buildListTile('System operacyjny', widget.details['system'],
-                        Icons.settings),
-                    buildListTile('Pojemność baterii',
-                        widget.details['battery'], Icons.battery_full),
-                    buildListTile('Wymiary', widget.details['sizes'],
-                        Icons.photo_size_select_large),
-                    buildListTile(
-                        'Waga', widget.details['weight'], Icons.fitness_center),
+                        'Pojemność baterii', widget.details['battery'], true),
+                    buildListTile('Wymiary', widget.details['sizes'], false),
+                    buildListTile('Waga', widget.details['weight'], true),
                   ] else if (widget.categoryId == 1) ...[
                     buildListTile(
-                        'Wyświetlacz', widget.details['display'], Icons.tv),
-                    buildListTile('Rodzaj ekranu',
-                        widget.details['display_type'], Icons.tv),
-                    buildListTile('Rozdzielczość', widget.details['resolution'],
-                        Icons.aspect_ratio),
+                        'Wyświetlacz', widget.details['display'], true),
+                    buildListTile(
+                        'Rodzaj ekranu', widget.details['display_type'], false),
+                    buildListTile(
+                        'Rozdzielczość', widget.details['resolution'], true),
                     buildListTile('Odświeżanie ekranu',
-                        widget.details['refreshing'], Icons.refresh),
+                        widget.details['refreshing'], false),
+                    buildListTile('Procesor', widget.details['cpu'], true),
                     buildListTile(
-                        'Procesor', widget.details['cpu'], Icons.memory),
-                    buildListTile(
-                        'Karta graficzna', widget.details['gpu'], Icons.memory),
-                    buildListTile(
-                        'Pamieć RAM', widget.details['ram'], Icons.memory),
+                        'Karta graficzna', widget.details['gpu'], false),
+                    buildListTile('Pamieć RAM', widget.details['ram'], true),
                     buildListTile('Rodzaj pamięci RAM',
-                        widget.details['ram_type'], Icons.memory),
+                        widget.details['ram_type'], false),
                     buildListTile('Pojemność i rodzaj dysku',
-                        widget.details['storage'], Icons.storage),
-                    buildListTile('System operacyjny', widget.details['system'],
-                        Icons.settings),
-                    buildListTile('Pojemność baterii',
-                        widget.details['battery'], Icons.battery_full),
-                    buildListTile('Wymiary', widget.details['sizes'],
-                        Icons.photo_size_select_large),
+                        widget.details['storage'], true),
                     buildListTile(
-                        'Waga', widget.details['weight'], Icons.fitness_center),
+                        'System operacyjny', widget.details['system'], false),
+                    buildListTile(
+                        'Pojemność baterii', widget.details['battery'], true),
+                    buildListTile('Wymiary', widget.details['sizes'], false),
+                    buildListTile('Waga', widget.details['weight'], true),
                   ] else if (widget.categoryId == 2) ...[
                     buildListTile('Współpracujący system operacyjny',
-                        widget.details['system'], Icons.settings),
-                    buildListTile('Wymiary', widget.details['sizes'],
-                        Icons.photo_size_select_large),
-                    buildListTile('Pulsometr', widget.details['heart_monitor'],
-                        Icons.photo_size_select_large),
+                        widget.details['system'], true),
+                    buildListTile('Wymiary', widget.details['sizes'], false),
                     buildListTile(
-                        'Akcelerometr',
-                        widget.details['accelerometer'],
-                        Icons.photo_size_select_large),
-                    buildListTile('Żyroskop', widget.details['gyroscope'],
-                        Icons.photo_size_select_large),
-                    buildListTile('Stoper', widget.details['stoper'],
-                        Icons.photo_size_select_large),
-                    buildListTile('Alarm', widget.details['alarm'],
-                        Icons.photo_size_select_large),
-                    buildListTile('Odbiornik GPS', widget.details['gps'],
-                        Icons.photo_size_select_large),
+                        'Pulsometr', widget.details['heart_monitor'], true),
                     buildListTile(
-                        'Informacje pogodowe',
-                        widget.details['weather_info'],
-                        Icons.photo_size_select_large),
+                        'Akcelerometr', widget.details['accelerometer'], false),
                     buildListTile(
-                        'Funkcja odbierania połączeń',
-                        widget.details['receiving_calls'],
-                        Icons.photo_size_select_large),
+                        'Żyroskop', widget.details['gyroscope'], true),
+                    buildListTile('Stoper', widget.details['stoper'], false),
+                    buildListTile('Alarm', widget.details['alarm'], true),
                     buildListTile(
-                        'System płatności',
-                        widget.details['payment_system'],
-                        Icons.photo_size_select_large),
+                        'Odbiornik GPS', widget.details['gps'], false),
+                    buildListTile('Informacje pogodowe',
+                        widget.details['weather_info'], true),
+                    buildListTile('Funkcja odbierania połączeń',
+                        widget.details['receiving_calls'], false),
+                    buildListTile('System płatności',
+                        widget.details['payment_system'], true),
                   ] else if (widget.categoryId == 3) ...[
+                    buildListTile('Procesor', widget.details['cpu'], true),
                     buildListTile(
-                        'Procesor', widget.details['cpu'], Icons.settings),
-                    buildListTile('Karta graficzna', widget.details['gpu'],
-                        Icons.settings),
+                        'Karta graficzna', widget.details['gpu'], false),
+                    buildListTile('Pamięć RAM', widget.details['ram'], true),
                     buildListTile(
-                        'Pamięć RAM', widget.details['ram'], Icons.settings),
-                    buildListTile('Pojemność dysku', widget.details['disc'],
-                        Icons.settings),
-                    buildListTile('Dysk optyczny',
-                        widget.details['optical_drive'], Icons.settings),
+                        'Pojemność dysku', widget.details['disc'], false),
                     buildListTile(
-                        'Wymiary', widget.details['size'], Icons.settings),
-                    buildListTile(
-                        'Waga', widget.details['weight'], Icons.settings),
-                    buildListTile(
-                        'Zasilanies', widget.details['power'], Icons.settings),
+                        'Dysk optyczny', widget.details['optical_drive'], true),
+                    buildListTile('Wymiary', widget.details['size'], false),
+                    buildListTile('Waga', widget.details['weight'], true),
+                    buildListTile('Zasilanies', widget.details['power'], false),
                   ] else if (widget.categoryId == 4) ...[
-                    buildListTile('Rozmiar ekranu', widget.details['display'],
-                        Icons.settings),
-                    buildListTile('Format ekranu', widget.details['format'],
-                        Icons.settings),
-                    buildListTile('Rozdzielczość', widget.details['resolution'],
-                        Icons.settings),
-                    buildListTile('Częstotliwość odświeżanie ekranu',
-                        widget.details['refresh_rate'], Icons.settings),
                     buildListTile(
-                        'Typ matrycy', widget.details['type'], Icons.settings),
+                        'Rozmiar ekranu', widget.details['display'], true),
+                    buildListTile(
+                        'Format ekranu', widget.details['format'], false),
+                    buildListTile(
+                        'Rozdzielczość', widget.details['resolution'], true),
+                    buildListTile('Częstotliwość odświeżanie ekranu',
+                        widget.details['refresh_rate'], false),
+                    buildListTile('Typ matrycy', widget.details['type'], true),
                     buildListTile('Czas reakcji matrycy',
-                        widget.details['reaction_time'], Icons.settings),
+                        widget.details['reaction_time'], false),
                     buildListTile('Liczba wyświetlanych kolorów',
-                        widget.details['number_of_colors'], Icons.settings),
-                    buildListTile('HDR', widget.details['hdr'], Icons.settings),
-                    buildListTile(
-                        'Wymiary', widget.details['size'], Icons.settings),
-                    buildListTile(
-                        'Weight', widget.details['weight'], Icons.settings),
+                        widget.details['number_of_colors'], true),
+                    buildListTile('HDR', widget.details['hdr'], false),
+                    buildListTile('Wymiary', widget.details['size'], true),
+                    buildListTile('Weight', widget.details['weight'], false),
                   ] else if (widget.categoryId == 5) ...[
-                    buildListTile('Rozmiar ekranu', widget.details['display'],
-                        Icons.settings),
-                    buildListTile('Rozdzielczość', widget.details['resolution'],
-                        Icons.settings),
+                    buildListTile(
+                        'Rozmiar ekranu', widget.details['display'], true),
+                    buildListTile(
+                        'Rozdzielczość', widget.details['resolution'], false),
                     buildListTile('Częstotliwość odświeżanie ekranu',
-                        widget.details['refresh_rate'], Icons.settings),
+                        widget.details['refresh_rate'], true),
+                    buildListTile('Typ matrycy', widget.details['type'], false),
+                    buildListTile('HDR', widget.details['hdr'], true),
                     buildListTile(
-                        'Typ matrycy', widget.details['type'], Icons.settings),
-                    buildListTile('HDR', widget.details['hdr'], Icons.settings),
-                    buildListTile(
-                        'Smart TV', widget.details['smart_tv'], Icons.settings),
-                    buildListTile(
-                        'System', widget.details['system'], Icons.settings),
-                    buildListTile(
-                        'Wi-fi', widget.details['wifi'], Icons.settings),
+                        'Smart TV', widget.details['smart_tv'], false),
+                    buildListTile('System', widget.details['system'], true),
+                    buildListTile('Wi-fi', widget.details['wifi'], false),
                     buildListTile('Montaż na ścianie',
-                        widget.details['wall_montage'], Icons.settings),
+                        widget.details['wall_montage'], true),
                     buildListTile('Wymiary z podstawą',
-                        widget.details['size_with_base'], Icons.settings),
+                        widget.details['size_with_base'], false),
                     buildListTile('Wymiary bez podstawy',
-                        widget.details['size_without_base'], Icons.settings),
+                        widget.details['size_without_base'], true),
                     buildListTile('Waga z podstawą',
-                        widget.details['weight_with_base'], Icons.settings),
+                        widget.details['weight_with_base'], false),
                     buildListTile('Waga bez podstawy',
-                        widget.details['weight_without_base'], Icons.settings),
+                        widget.details['weight_without_base'], true),
                   ],
                 ],
               ),
@@ -263,15 +257,29 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
           if (widget.routeName != '/shoppingCartPage' &&
               widget.routeName != '/ordersPage')
             Container(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(12.0), // Dodaj Padding dookoła
               color: Colors.orange,
               child: GestureDetector(
                 onTap: () {
                   addToShoppingCart(widget.productId);
                 },
-                child: const ListTile(
-                  leading: Icon(Icons.shopping_cart),
-                  title: Text('Dodaj do koszyka'),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 8.0),
+                    Text(
+                      'Dodaj do koszyka',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
