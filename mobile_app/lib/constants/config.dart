04b80e17bbdc955c7ac03e8a418c6adcf2b8d7cc
ejsiik <ejsik.lel@gmail.com>
2024-01-17 14:03:07 +0100

    adminId

--- mobile_app/lib/constants/config.dart
+++ mobile_app/lib/constants/config.dart
@@ -2,5 +2,5 @@ const List<String> adminEmail = [
   "daw.wydra@gmail.com",
   "awjajecznik@gmail.com",
 ];
-const String receiverId = 'jlZWkZEd2aeZKfSVU7p0iIzLhLr2';
+const String receiverId = '8Gz0rKUR7TeRctkPqt8a8D4D3ID3';
 const String receiverEmail = 'daw.wydra@gmail.com';
