39d49209ec15130cb96bc3077c1ac13bdf5a2be4
Marcin Leśniak <mcjajecznik@gmail.com>
2024-02-06 17:03:27 +0100

    Update config.dart

--- mobile_app/lib/constants/config.dart
+++ mobile_app/lib/constants/config.dart
@@ -1,6 +1,5 @@
 const List<String> adminEmail = [
   "daw.wydra@gmail.com",
-  "awjajecznik@gmail.com",
 ];
 const String receiverId = '8Gz0rKUR7TeRctkPqt8a8D4D3ID3';
 const String receiverEmail = 'daw.wydra@gmail.com';
